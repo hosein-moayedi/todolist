eab4eeb227d87460d3fc29bf4dd93348c1c76755
hosein <hoseinmoayedi98@gmail.com>
2024-01-03 14:00:24 +0330

    add global error handler func

--- src/services/api/helpers.ts
+++ src/services/api/helpers.ts
@@ -1,18 +1,16 @@
-//@ts-nocheck
+//@ts-noCheck
+import {isRejectedWithValue} from '@reduxjs/toolkit';
 import {FetchBaseQueryError} from '@reduxjs/toolkit/query';
+import {Alert} from 'react-native';
+import RNRestart from 'react-native-restart';
+import STRINGS from '../../localization';
 
-/**
- * Type predicate to narrow an unknown error to `FetchBaseQueryError`
- */
 export function isFetchBaseQueryError(
   error: unknown,
 ): error is FetchBaseQueryError {
   return typeof error === 'object' && error != null && 'status' in error;
 }
 
-/**
- * Type predicate to narrow an unknown error to an object with a string 'message' property
- */
 export function isErrorWithMessage(error: unknown): error is {message: string} {
   return (
     typeof error === 'object' &&
@@ -28,5 +26,49 @@ export function getErrorMessage(error) {
     return errMsg;
   } else if (isErrorWithMessage(error)) {
     return error.message;
+  }
+}
+
+export function processServerResponseErrors(action) {
+  if (isRejectedWithValue(action)) {
+    const {status} = action.payload;
+    if (status >= 400) {
+      switch (true) {
+        case status == 400:
+          Alert.alert(
+            STRINGS.errors.server.default.ErrorMessage,
+            getErrorMessage(action.payload),
+          );
+          break;
+        case status == 401:
+          RNRestart.restart();
+          break;
+        case status == 403:
+          Alert.alert(
+            STRINGS.errors.server.default.AccessDenied,
+            STRINGS.errors.server.default.DoNotHavePermission,
+          );
+          break;
+        case status == 404:
+          Alert.alert(
+            STRINGS.errors.server.default.ResourceNotFound,
+            STRINGS.errors.server.default
+              .RequestedResourceNotFoundPleaseTryAgain,
+          );
+          break;
+        case status >= 500:
+          Alert.alert(
+            STRINGS.errors.server.default.SomethingWentWrong,
+            STRINGS.errors.server.default.SomethingWrongTryAgainLater,
+          );
+          break;
+        default:
+          Alert.alert(
+            STRINGS.errors.server.default.UnknownError,
+            STRINGS.errors.server.default.SomethingWrongTryAgainLater,
+          );
+          break;
+      }
+    }
   }
 }
