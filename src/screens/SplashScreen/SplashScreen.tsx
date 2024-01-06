45337693e0d15d12575a9844673dde3abaa8f151
hosein <hoseinmoayedi98@gmail.com>
2024-01-06 15:30:28 +0330

    refactor splash screen

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -1,48 +1,61 @@
-import React, { useEffect } from 'react'
-import { Text, View } from 'react-native'
-import * as Keychain from 'react-native-keychain'
-import RNRestart from 'react-native-restart'
-import { useDispatch } from 'react-redux'
-import STRINGS from '../../localization'
-import { setAppReadyState } from '../../redux/app'
-import { setAuthorizedState, setTokensState } from '../../redux/auth'
-import { useRefreshTokenMutation } from '../../services/api/user'
-import debug from '../../utils/debug'
-import { styles } from './styles'
+import React, { FC, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import Keychain from 'react-native-keychain';
+import RNRestart from 'react-native-restart';
+import { useDispatch } from 'react-redux';
+import STRINGS from '../../localization';
+import { setAppReadyState } from '../../redux/app';
+import { setAuthorizedState, setTokensState } from '../../redux/auth';
+import { useRefreshTokenMutation } from '../../services/api/user';
+import debug from '../../utils/debug';
+import { styles } from './styles';
 
 
-export default function SplashScreen() {
-    const dispatch = useDispatch()
-    const [refreshTokenAPI,] = useRefreshTokenMutation()
+const SplashScreen: FC = () => {
+    const dispatch = useDispatch();
+    const [refreshTokenAPI] = useRefreshTokenMutation();
+
+    const handleTokenRefresh = async () => {
+        try {
+            const credentials = await Keychain.getGenericPassword();
+
+            if (credentials) {
+                const tokens = JSON.parse(credentials.password);
+                const nowTime = new Date().getTime();
+
+                if (tokens.expiry <= nowTime) {
+                    const newTokens = await refreshTokenAPI({ refreshToken: tokens.refresh }).unwrap();
+                    await Keychain.setGenericPassword(credentials.username, JSON.stringify(newTokens.tokens));
+                }
+
+                dispatch(setTokensState(tokens));
+                dispatch(setAuthorizedState(true));
+            }
+        } catch (error) {
+            debug.error(error);
+            await Keychain.resetGenericPassword();
+            RNRestart.restart();
+        }
+    };
+
+    const handleAppReady = () => {
+        setTimeout(() => dispatch(setAppReadyState(true)), 1000);
+    };
 
     useEffect(() => {
-        (async () => {
-            try {
-                const credentials = await Keychain.getGenericPassword()
-                if (credentials) {
-                    const tokens = await JSON.parse(credentials.password)
-                    const nowTime = new Date().getTime()
+        const fetchData = async () => {
+            await handleTokenRefresh();
+            handleAppReady();
+        };
 
-                    if (tokens.expiry <= nowTime) {
-                        const newTokens = await refreshTokenAPI({ refreshToken: tokens.refresh }).unwrap()
-                        await Keychain.setGenericPassword(credentials.username, JSON.stringify(newTokens.tokens))
-                    }
-
-                    dispatch(setTokensState(tokens))
-                    dispatch(setAuthorizedState(true))
-                }
-                setTimeout(() => dispatch(setAppReadyState(true)), 1000)
-            } catch (error) {
-                debug.error(error);
-                await Keychain.resetGenericPassword()
-                RNRestart.restart()
-            }
-        })()
-    }, [])
+        fetchData();
+    }, []);
 
     return (
         <View style={styles.viewContainer}>
             <Text>{STRINGS.screens.splash.SplashScreen}</Text>
         </View>
-    )
-}
+    );
+};
+
+export default SplashScreen;
